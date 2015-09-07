6be4005011d552115b68c147646fd0f55a1ac302
Elex Lee <Elex@Elexs-MBP.hsd1.ca.comcast.net>
2015-09-07 15:38:48 -0700

    Finished project 14

---
+++ Project14/Project14/Project14/Project14/Project14/Project14/Project14/Project14/Project14/Project14/Project14/Project14/Project14/Project14/Project14/Project14/Project14/Project14/Project14/Project14/Project14/Project14/Project14/Project14/Project14/Project14/Project14/Project14/Project14/Project14/Project14/Project14/Project14/Project14/Project14/Project14/GameScene.swift
@@ -0,0 +1,23 @@
+//
+//  GameScene.swift
+//  Project14
+//
+//  Created by Elex Lee on 9/7/15.
+//  Copyright (c) 2015 Elex Lee. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        /* Called when a touch begins */
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
